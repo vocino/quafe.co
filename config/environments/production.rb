a7ca12bba30cd4c031644f28c40ef3df8c91d2a4
Travis Vocino <travis@vocino.com>
2014-02-07 15:30:13 -0800

    devise, dashboard, users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ QuafeCo::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'zero.quafe.co' }
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-QuafeCo::Application.config.secret_key_base = 'ddb73a0c073e4608374bc8bab6bba7d2515f58eebeab5f11ad1b743440035f5881188905ddb7a01f0d4e8ce50764b9d65602da783f50fd1ba6a1fe537730f25b'
+QuafeCo::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
